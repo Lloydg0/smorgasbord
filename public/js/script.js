12bc5b97eb1fda1e5b9b3ee41f806e04b7e6943d
Lloyd Grogan <lloydgrogan@hotmail.com>
2021-04-30 13:14:28 +0200

    Week 8 (Friday Morning) Extra styling and partial img upload

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,10 @@ new Vue({
     el: "#main",
     data: {
         images: [],
+        title: "",
+        description: "",
+        username: "",
+        file: null,
     },
     mounted: function () {
         // var scroll = this;
@@ -15,13 +19,42 @@ new Vue({
         // });
         console.log("this in mounted", this);
         axios.get("/imageboard").then((response) => {
-            console.log("Response in axios", response);
-            console.log("Response in axios", response.data);
+            // console.log("Response in axios", response);
+            // console.log("Response in axios", response.data);
             this.images = response.data;
-            console.log("this in axios", this);
+            // console.log("this in axios", this);
         });
     },
-    method: {
-        // fadeOut: function () {},
+    methods: {
+        handleChange: function (e) {
+            // console.log(" e in handle Change is running", e.target.files[0]);
+            this.file = e.target.files[0];
+        },
+        submitFile: function (e) {
+            // e.preventDefault();
+            console.log("e in submitFile is running");
+            //were going to user an API called FormData to send the file to the server (formData is only for files)
+            var formData = new FormData();
+            formData.append("file", this.file);
+            //makes sense to have other data in one big object with the file
+            formData.append("title", this.title);
+            formData.append("description", this.description);
+            formData.append("username", this.username);
+            //formData always logd and empty object, that mean it didnt work
+            // console.log("formdata", formData);
+
+            /// next step is to send formdata off to the server (all communcaiton with client and server happens with axios in vue)
+
+            //what ever you add as the second arguement in axios.post is going to end up in server.js in req.body
+            //second argument must be an object
+            axios
+                .post("/upload", formData)
+                .then(() => {
+                    console.log("response recievedf from server");
+                })
+                .catch((err) => {
+                    console.log("err in POST/upload", err);
+                });
+        },
     },
 });
--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
 const db = require("./sql/db.js");
+
+//the following ocde is required to uoload files
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152, //files over 2mb cannot be uploaded used to stop ddos attacks (if upload does not work, check the size of the file as it might be too big and not a bug in the code).
+    },
+});
+////// end of code that uploads the files
+
+//getting static folders
 app.use(express.static("public"));
 
+//get request to get images from DB
 app.get("/imageboard", (req, res) => {
     db.getImagesDataBaseInformation()
         .then((result) => {
@@ -12,6 +39,29 @@ app.get("/imageboard", (req, res) => {
         .catch((err) => {
             console.log("ERROR in retrieving Information from Database", err);
         });
+});
+
+//POST request to uploads the images
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("upload Worked!!!!");
+    //insert into DB  (filename, title, description, username)
+    console.log("req.body", req.body);
+    console.log("req.file", req.file); // req.file comes fom multer
+    if (req.file) {
+        // this will run if everything works
+        //inster into images
+        // send back a response using res.json
+        res.json({
+            success: true,
+        });
+    } else {
+        //this runs if something broke
+        // send back a response to vue using res.json
+        // the response we send back needs to be something that indicates the upload didnt work
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080, () => console.log("Image board listening on 8080!!"));
